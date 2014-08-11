9420f4a9ba0f113a352d65a44b8372a42f27b501
135yshr <isago@praisefirst.co.jp>
2014-08-11 14:05:10 +0900

    added to create instance testcase.

--- scratch2hue.go
+++ scratch2hue.go
@@ -1,14 +1,14 @@
 package scratch2hue
 
 import (
-	"fmt"
-	scratch "github.com/135yshr/scratchgo"
-	"github.com/savaki/go.hue"
+// "fmt"
+// scratch "github.com/135yshr/scratchgo"
+// "github.com/savaki/go.hue"
 )
 
 type HueConnection struct {
 }
 
 func NewConnection(ipaddr string) (*HueConnection, error) {
-	return nil, nil
+	return &HueConnection{}, nil
 }
--- scratch2hue_test.go
+++ scratch2hue_test.go
@@ -9,7 +9,8 @@ func TestScaratch2Hue(t *testing.T) {
 	Describe(t, "Scratch2Hue TestCase", func() {
 		Context("connect to hue", func() {
 			It("create new conneciton.", func() {
-				actual := NewConnection("localhost")
+				actual, err := NewConnection("localhost")
+				Expect(err).To(NotExist)
 				Expect(actual).To(Exist)
 			})
 		})
