b1004e9ec2e187477a19cec456bda7c84aa54f43
135yshr <isago@praisefirst.co.jp>
2014-08-12 15:04:15 +0900

    changed for go.hue to scratch2hue

--- sample/main.go
+++ sample/main.go
@@ -4,122 +4,39 @@ import (
 	"flag"
 	"fmt"
 	scratch "github.com/135yshr/scratchgo"
-	"github.com/savaki/go.hue"
 	"os"
+	s2hue "scratch2hue"
 )
 
 var ipaddr string
-var state hue.SetLightState
-var id string
 
 func main() {
 
 	flag.Parse()
 
-	conn, bridge := initialize_routine()
+	if ipaddr == "" {
+		fmt.Println("set ipaddrss")
+		os.Exit(1)
+	}
+
+	conn, err := scratch.NewDefaultConnect()
+	errorToExit("connect to scratch.", err)
+
+	hue := s2hue.NewConnection(ipaddr)
 	for {
 		msg, err := conn.Recv()
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
-		fmt.Println(*msg)
-		action(msg, bridge)
+		hue.Anction(msg)
 	}
 
 	os.Exit(0)
 }
 
 func init() {
-	flag.StringVar(&ipaddr, "ip", "not found", "bridge ip address.")
-}
-
-func action(msg *scratch.Message, bridge *hue.Bridge) {
-	switch msg.Type {
-	case "broadcast":
-		light, err := bridge.FindLightById(id)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		switch msg.Variables["command"] {
-		case "action":
-			results, err := light.SetState(state)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(results)
-
-		case "discotime":
-			results, err := light.ColorLoop()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(results)
-
-		case "light_on":
-			results, err := light.On()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(results)
-
-		case "light_off":
-			results, err := light.Off()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println(results)
-
-		case "light_all_on":
-			lights, err := bridge.GetAllLights()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			for _, l := range lights {
-				results, err := l.On()
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				fmt.Println(results)
-			}
-		case "light_all_off":
-			lights, err := bridge.GetAllLights()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			for _, l := range lights {
-				results, err := l.Off()
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				fmt.Println(results)
-			}
-			fmt.Println("success!")
-		}
-	case "sensor-update":
-		val := msg.Variables
-		for _, v := range msg.GetNames() {
-			switch v {
-			case "on":
-				state.On = val["on"]
-			case "brightness":
-				state.Bri = val["brightness"]
-			case "color":
-				state.Hue = val["color"]
-			case "id":
-				id = val["id"]
-			}
-		}
-	}
+	flag.StringVar(&ipaddr, "ip", "", "bridge ip address.")
 }
 
 func errorToExit(message string, err error) {
@@ -128,22 +45,4 @@ func errorToExit(message string, err error) {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func errorToConsole(err error) {
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func initialize_routine() (*scratch.RspConn, *hue.Bridge) {
-	fmt.Println("now during initializing...")
-
-	conn, err := scratch.NewDefaultConnect()
-	errorToExit("connect to scratch.", err)
-
-	bridge := hue.NewBridge(ipaddr, "newdeveloper")
-	fmt.Println("registered new device =>", *bridge)
-
-	return conn, bridge
 }
